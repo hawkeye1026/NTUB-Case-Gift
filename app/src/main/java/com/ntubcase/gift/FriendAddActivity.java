882efcfcef20b6e2be940c494cdf5f64000d75e0
tzu-tzu <st930012@gmail.com>
2019-09-01 12:09:07 +0800

    friend: insert

--- app/src/main/java/com/ntubcase/gift/FriendAddActivity.java
+++ app/src/main/java/com/ntubcase/gift/FriendAddActivity.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ntubcase.gift.Common.Common;
+import com.ntubcase.gift.MyAsyncTask.friend.friendInsertAsyncTask;
 import com.ntubcase.gift.MyAsyncTask.friend.friendSearchAsyncTask;
 import com.squareup.picasso.Picasso;
 
@@ -33,7 +34,7 @@ public class FriendAddActivity extends AppCompatActivity {
     Button btnAdd,btnSearch;
 
     //---
-    private String userid="1";
+    private String userid="1", friendid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,51 +50,77 @@ public class FriendAddActivity extends AppCompatActivity {
         imgFriend = (ImageView) findViewById(R.id.img_Friend);//好友照片
         txtFriend = (TextView) findViewById(R.id.txt_Friend);//好友帳號
         btnSearch = (Button) findViewById(R.id.btnSearch);//加入按鈕
-        btnAdd = (Button) findViewById(R.id.btnAdd);//加入按鈕
+        btnAdd = (Button) findViewById(R.id.btn_add);//加入按鈕
 
-        //-----製作計畫按鈕-----
+        //--搜尋
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                friendSearchAsyncTask friendSearchAsyncTask = new friendSearchAsyncTask(new friendSearchAsyncTask.TaskListener() {
-                    @Override
-                    public void onFinished(String result) {
-                        try {
-                            if (result == null) {
-                                Toast.makeText(FriendAddActivity.this,"無資料!", Toast.LENGTH_SHORT).show();
-                                return;
+                if(edt_sEmail.getText().toString().equals("")){
+                    Toast.makeText(FriendAddActivity.this,"輸入好友信箱!", Toast.LENGTH_SHORT).show();
+                }else {
+
+                    friendSearchAsyncTask friendSearchAsyncTask = new friendSearchAsyncTask(new friendSearchAsyncTask.TaskListener() {
+                        @Override
+                        public void onFinished(String result) {
+                            try {
+                                if (result == null) {
+                                    Toast.makeText(FriendAddActivity.this,"無資料!", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                                JSONObject object = new JSONObject(result);
+
+                                //取得禮物紀錄
+                                JSONArray jsonArray = object.getJSONArray("result");
+                                int resultLength = jsonArray.length();
+                                Log.v("resultLength", String.valueOf(resultLength));
+
+                                friendid =jsonArray.getJSONObject(0).getString("userid");
+                                String nickname =jsonArray.getJSONObject(0).getString("nickname");
+                                String mail =jsonArray.getJSONObject(0).getString("mail");
+                                String image =jsonArray.getJSONObject(0).getString("image");
+
+                                //-------圖片網址 getGift(n) 取得第n筆資料的禮物資料
+                                Uri imageURI = Uri.parse(image);
+                                if (imageURI!=null){
+                                    Picasso.get().load(imageURI)
+                                            .transform(new CircleTransform())
+                                            .into(imgFriend);
+                                }
+
+                                txtFriend.setText(nickname);
+
+                            } catch (Exception e) {
+                                Toast.makeText(FriendAddActivity.this, "連線失敗!", Toast.LENGTH_SHORT).show();
                             }
-                            JSONObject object = new JSONObject(result);
-
-                            //取得禮物紀錄
-                            JSONArray jsonArray = object.getJSONArray("result");
-                            int resultLength = jsonArray.length();
-                            Log.v("resultLength", String.valueOf(resultLength));
-
-                            String nickname =jsonArray.getJSONObject(0).getString("nickname");
-                            String mail =jsonArray.getJSONObject(0).getString("mail");
-                            String image =jsonArray.getJSONObject(0).getString("image");
-
-                            //-------圖片網址 getGift(n) 取得第n筆資料的禮物資料
-                            Uri imageURI = Uri.parse(image);
-                            if (imageURI!=null){
-                                Picasso.get().load(imageURI)
-                                        .transform(new CircleTransform())
-                                        .into(imgFriend);
-                            }
-
-                            txtFriend.setText(nickname);
-
-                        } catch (Exception e) {
-                            Toast.makeText(FriendAddActivity.this, "連線失敗!", Toast.LENGTH_SHORT).show();
                         }
-                    }
-                });
-                Log.v("mail", edt_sEmail.getText().toString());
-                friendSearchAsyncTask.execute(Common.friendQuery , userid, edt_sEmail.getText().toString());
+                    });
+                    Log.v("mail", edt_sEmail.getText().toString());
+                    friendSearchAsyncTask.execute(Common.friendQuery , userid, edt_sEmail.getText().toString());
 
+                }
             }
         });
+
+        //--加入
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!(friendid == null)){
+                    friendInsertAsyncTask friendInsertAsyncTask = new friendInsertAsyncTask(new friendInsertAsyncTask.TaskListener() {
+                        @Override
+                        public void onFinished(String result) {
+                            Toast.makeText(FriendAddActivity.this, "加入成功", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    Log.v("friendid", friendid);
+                    friendInsertAsyncTask.execute(Common.insertFriend , userid, friendid);
+                }else {
+                    Toast.makeText(FriendAddActivity.this, "無資料", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
     @Override
