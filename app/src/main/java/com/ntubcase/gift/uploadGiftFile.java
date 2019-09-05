a34317fe59de8e5ce0efd065319f255625fb0dff
wayne <linchengway000@gmail.com>
2019-09-05 18:12:10 +0800

    insert update gift success

--- app/src/main/java/com/ntubcase/gift/uploadGiftFile.java
+++ app/src/main/java/com/ntubcase/gift/uploadGiftFile.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 
 import com.ntubcase.gift.Common.Common;
 import com.ntubcase.gift.MyAsyncTask.gift.insert.giftInsertImg_imageAsyncTask;
+import com.ntubcase.gift.MyAsyncTask.gift.insert.giftInsertImg_infoAsyncTask;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -13,17 +14,23 @@ public class uploadGiftFile {
 
     public uploadGiftFile(Context context, Uri cam_imageUri, String giftContent, String old_giftContent, String owner,String fileType,String crud){
         //--------取得目前時間：yyyy/MM/dd hh:mm:ss
-        Date date =new Date();
-        SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm");
-        String dateTime = sdFormat.format(date);
 //        Log.v("gift",giftContent+"||"+giftName+"||"+owner+"||"+giftType);
-        //------------------------------上傳禮物資料
+        //------------------------------上傳禮物檔案
         giftInsertImg_imageAsyncTask mGiftInsertImgAsyncTask = new giftInsertImg_imageAsyncTask(new giftInsertImg_imageAsyncTask.TaskListener() {
             @Override
             public void onFinished(String result) {
 
             }
         }, ImageFilePath.getPath(context, cam_imageUri));
-        mGiftInsertImgAsyncTask.execute(Common.insertGiftUploadGiftFile, giftContent, old_giftContent,owner,fileType,crud);
+        mGiftInsertImgAsyncTask.execute(Common.insertGiftUploadGiftFile,giftContent,fileType);
+
+        //------------------------------上傳禮物資訊
+        giftInsertImg_infoAsyncTask mGiftInsertImg_infoAsyncTask = new giftInsertImg_infoAsyncTask(new giftInsertImg_infoAsyncTask.TaskListener() {
+            @Override
+            public void onFinished(String result) {
+
+            }
+        });
+        mGiftInsertImg_infoAsyncTask.execute(Common.insertGiftUploadGiftFile, giftContent, old_giftContent,owner,fileType,crud);
     }
 }
