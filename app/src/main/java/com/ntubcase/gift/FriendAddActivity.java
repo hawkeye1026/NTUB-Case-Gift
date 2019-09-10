f7bdb15be1cd1893dc1b91d6cde09bf02936a6ac
YJL <hawkeye861026@gmail.com>
2019-09-11 00:12:59 +0800

    Scanner move to FriendAdd, Friend can't repeat or add self

--- app/src/main/java/com/ntubcase/gift/FriendAddActivity.java
+++ app/src/main/java/com/ntubcase/gift/FriendAddActivity.java
@@ -1,5 +1,6 @@
 package com.ntubcase.gift;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -14,6 +16,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.ntubcase.gift.Common.Common;
 import com.ntubcase.gift.MyAsyncTask.friend.friendInsertAsyncTask;
 import com.ntubcase.gift.MyAsyncTask.friend.friendSearchAsyncTask;
@@ -22,19 +26,15 @@ import com.squareup.picasso.Picasso;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import com.ntubcase.gift.data.getFriendList;
 
-import static com.ntubcase.gift.MakePlanSingleActivity.edt_single_friend;
 
 public class FriendAddActivity extends AppCompatActivity {
-    EditText edt_sEmail;
-    ImageView imgFriend;
-    TextView txtFriend;
-    Button btnAdd,btnSearch;
-    LinearLayout find_friend;
-
+    private EditText edt_sEmail;
+    private ImageView imgFriend;
+    private TextView txtFriend;
+    private Button btnAdd, btnSearch, btn_scan;
+    private LinearLayout find_friend;
     //---
     private String userid="1", friendid;
 //    private String userid="1", friendid;
@@ -50,64 +50,47 @@ public class FriendAddActivity extends AppCompatActivity {
         //-----------------------------------------------------
 
         edt_sEmail = (EditText) findViewById(R.id.edt_sEmail); //搜尋的email
+        btn_scan = (Button) findViewById(R.id.iv_scanner); //QRcode掃描器
+        find_friend =(LinearLayout) findViewById(R.id.find_friend);
         imgFriend = (ImageView) findViewById(R.id.img_Friend);//好友照片
         txtFriend = (TextView) findViewById(R.id.txt_Friend);//好友帳號
         btnSearch = (Button) findViewById(R.id.btnSearch);//搜尋按鈕
         btnAdd = (Button) findViewById(R.id.btn_add);//加入按鈕
-        find_friend =(LinearLayout) findViewById(R.id.find_friend);
 
-        //--搜尋
+
+        //-----搜尋-----
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(edt_sEmail.getText().toString().equals("")){
                     Toast.makeText(FriendAddActivity.this,"輸入好友信箱!", Toast.LENGTH_SHORT).show();
                 }else {
-
-                    friendSearchAsyncTask friendSearchAsyncTask = new friendSearchAsyncTask(new friendSearchAsyncTask.TaskListener() {
-                        @Override
-                        public void onFinished(String result) {
-                            try {
-                                if (result == null) {
-                                    Toast.makeText(FriendAddActivity.this,"無資料!", Toast.LENGTH_SHORT).show();
-                                    return;
-                                }
-                                JSONObject object = new JSONObject(result);
-                                
-                                //取得禮物紀錄
-                                JSONArray jsonArray = object.getJSONArray("result");
-                                int resultLength = jsonArray.length();
-                                Log.v("resultLength", String.valueOf(resultLength));
-
-                                friendid =jsonArray.getJSONObject(0).getString("userid");
-                                String nickname =jsonArray.getJSONObject(0).getString("nickname");
-                                String mail =jsonArray.getJSONObject(0).getString("mail");
-                                String image =jsonArray.getJSONObject(0).getString("image");
-
-                                //-------圖片網址 getGift(n) 取得第n筆資料的禮物資料
-                                Uri imageURI = Uri.parse(image);
-                                if (imageURI!=null && !imageURI.toString().equals("")){
-                                    Picasso.get().load(imageURI)
-                                            .transform(new CircleTransform())
-                                            .into(imgFriend);
-                                }
-
-                                txtFriend.setText(nickname);
-                                find_friend.setVisibility(View.VISIBLE);
-                            } catch (Exception e) {
-                                Toast.makeText(FriendAddActivity.this, "沒找到此email的使用者!", Toast.LENGTH_SHORT).show();
-                                find_friend.setVisibility(View.INVISIBLE);
-                            }
-                        }
-                    });
-                    Log.v("mail", edt_sEmail.getText().toString());
-                    friendSearchAsyncTask.execute(Common.friendQuery , userid, edt_sEmail.getText().toString());
-
+                    searchFriend(edt_sEmail.getText().toString()); //搜尋好友
+                    //關閉鍵盤
+                    InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
                 }
             }
         });
 
-        //--加入
+        //-----QRcode掃描器-----
+        btn_scan.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                IntentIntegrator integrator = new IntentIntegrator(FriendAddActivity.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("請掃描好友的QRcode");
+                integrator.setOrientationLocked(false);
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(true);
+                integrator.setBarcodeImageEnabled(true);
+                integrator.initiateScan();
+            }
+        });
+
+        //-----加入-----
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -128,6 +111,81 @@ public class FriendAddActivity extends AppCompatActivity {
 
     }
 
+    //----------------------搜尋好友----------------------
+    private void searchFriend(final String friendEmail){
+        friendSearchAsyncTask friendSearchAsyncTask = new friendSearchAsyncTask(new friendSearchAsyncTask.TaskListener() {
+            @Override
+            public void onFinished(String result) {
+                try {
+                    if (result == null) {
+                        Toast.makeText(FriendAddActivity.this,"無資料!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    JSONObject object = new JSONObject(result);
+
+                    //取得禮物紀錄
+                    JSONArray jsonArray = object.getJSONArray("result");
+                    int resultLength = jsonArray.length();
+                    Log.v("resultLength", String.valueOf(resultLength));
+
+                    friendid =jsonArray.getJSONObject(0).getString("userid");
+                    String nickname =jsonArray.getJSONObject(0).getString("nickname");
+                    String mail =jsonArray.getJSONObject(0).getString("mail");
+                    String image =jsonArray.getJSONObject(0).getString("image");
+
+                    //-------圖片網址---------
+                    Uri imageURI = Uri.parse(image);
+                    if (imageURI!=null && !imageURI.toString().equals("")){
+                        Picasso.get().load(imageURI)
+                                .transform(new CircleTransform())
+                                .into(imgFriend);
+                    }
+
+                    txtFriend.setText(nickname);
+                    find_friend.setVisibility(View.VISIBLE); //顯示好友資訊區塊
+
+                    //-----檢查好友是否加過-----
+                    int i;
+                    for (i=0; i<getFriendList.getFriendLength(); i++){
+                        if(friendid.equals(getFriendList.getFriendid(i))) break;
+                    }
+                    if (friendid.equals(userid)) {  //自己
+                        btnAdd.setText("您本人");
+                        btnAdd.setEnabled(false);
+                    }else if (i==getFriendList.getFriendLength()){ //尚未加過
+                        btnAdd.setText("加入");
+                        btnAdd.setEnabled(true);
+                    }else{  //已加過
+                        btnAdd.setText("已加入");
+                        btnAdd.setEnabled(false);
+                    }
+
+                } catch (Exception e) {
+                    find_friend.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+        friendSearchAsyncTask.execute(Common.friendQuery , userid, friendEmail);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if (result!= null) {
+            if (result.getContents()==null) {
+                Toast.makeText(this, "取消掃描", Toast.LENGTH_SHORT).show();
+            }else {
+                String contents = data.getStringExtra("SCAN_RESULT");   //取得QR Code內容
+                searchFriend("linchengway000@gmail.com"); //***測試資料***
+            }
+        }else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+    }
+
+    //-------------------------------------------------------------------------------------------------
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == android.R.id.home){ //toolbar返回建
--- app/src/main/java/com/ntubcase/gift/SettingActivity.java
+++ app/src/main/java/com/ntubcase/gift/SettingActivity.java
@@ -1,10 +1,8 @@
 package com.ntubcase.gift;
 
-import android.app.Activity;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.content.Intent;
-import android.media.Image;
 
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,17 +11,11 @@ import android.util.Log;
 import android.view.MenuItem;
 
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.facebook.login.LoginManager;
 import com.google.zxing.WriterException;
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
-import com.ntubcase.gift.Common.Common;
-import com.ntubcase.gift.MyAsyncTask.friend.friendInsertAsyncTask;
 import com.ntubcase.gift.login_model.facebookAccount;
 import com.ntubcase.gift.login_model.googleAccount;
 import com.ntubcase.gift.login_model.signOut;
@@ -33,13 +25,13 @@ import com.squareup.picasso.Picasso;
 import androidmads.library.qrgenearator.QRGContents;
 import androidmads.library.qrgenearator.QRGEncoder;
 
+
 public class SettingActivity extends AppCompatActivity {
 
     ImageView mUserPhoto;
     TextView mNickname,mMail,mBirthday;
 
     ImageView mQrcode,mLogout;
-    Button mScanner;
 
     String ownerid = "1";
 
@@ -71,26 +63,8 @@ public class SettingActivity extends AppCompatActivity {
 
         //------設置
         mLogout = (ImageView)findViewById(R.id.iv_logout);
-
         mQrcode = (ImageView)findViewById(R.id.iv_qrcode);
 
-        mScanner = (Button) findViewById(R.id.iv_scanner);
-        final Activity activity = this;
-        mScanner.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                IntentIntegrator integrator = new IntentIntegrator(activity);
-                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
-                integrator.setPrompt("請掃描好友的QRcode");
-                integrator.setOrientationLocked(false);
-                integrator.setCameraId(0);
-                integrator.setBeepEnabled(true);
-                integrator.setBarcodeImageEnabled(true);
-                integrator.initiateScan();
-            }
-        });
 
         String protal = userData.getLoginProtal();
 
@@ -135,39 +109,6 @@ public class SettingActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true); //啟用返回建
         //------------------------------------------------------------------------------------------
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
-        if (result!= null)
-        {
-            if (result.getContents()==null)
-            {
-                Toast.makeText(this, "You cancelled the scanning", Toast.LENGTH_SHORT).show();
-            }
-            else
-            {
-                String contents = data.getStringExtra("SCAN_RESULT");   //取得QR Code內容
-
-                friendInsertAsyncTask friendInsertAsyncTask = new friendInsertAsyncTask(new friendInsertAsyncTask.TaskListener() {
-                    @Override
-                    public void onFinished(String result) {
-                        Toast.makeText(SettingActivity.this, "加入成功", Toast.LENGTH_SHORT).show();
-                    }
-                });
-                friendInsertAsyncTask.execute(Common.insertFriend , ownerid, contents);
-
-            }
-        }
-        else
-        {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-
-
 
     }
 
